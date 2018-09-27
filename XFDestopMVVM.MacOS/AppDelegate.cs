adc017bdc89b28074f6dc03f920ba454b0af553c
histech <histech@hotmail.fr>
2018-09-27 05:53:58 -0400

    Update MacOS project to support Xamarin.Forms

--- XFDestopMVVM.MacOS/AppDelegate.cs
+++ XFDestopMVVM.MacOS/AppDelegate.cs
@@ -1,18 +1,33 @@
 ﻿using AppKit;
 using Foundation;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.MacOS;
+using XFDesktopMVVM;
 
 namespace XFDestopMVVM.MacOS
 {
     [Register("AppDelegate")]
-    public class AppDelegate : NSApplicationDelegate
+    public class AppDelegate : FormsApplicationDelegate
     {
+        NSWindow window;
         public AppDelegate()
         {
+            var style = NSWindowStyle.Closable | NSWindowStyle.Resizable | NSWindowStyle.Titled;
+
+            var rect = new CoreGraphics.CGRect(200, 1000, 1024, 768);
+
+            window = new NSWindow(rect, style, NSBackingStore.Buffered, false);
+            window.Title = "FakeNewsFeed";
         }
+
+        public override NSWindow MainWindow => window;
 
         public override void DidFinishLaunching(NSNotification notification)
         {
             // Insert code here to initialize your application
+            Forms.Init();
+            LoadApplication(new App());
+            base.DidFinishLaunching(notification);
         }
 
         public override void WillTerminate(NSNotification notification)
